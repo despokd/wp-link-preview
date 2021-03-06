f83abf0d6818ae3506ccc26304918f7707633947
Kilian Domaratius <github@tequilian.de>
2021-03-06 12:57:18 +0100

    Init as WordPress plugin

---
+++ tequilian-link-preview/tequilian-link-preview.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin Name
+ *
+ * @package           TequilianLinkPreview
+ * @author            Kilian Domaratius
+ * @copyright         2021 Kilian Domaratius
+ * @license           MIT License
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Link Preview of Tequilian
+ * Plugin URI:        https://tequilian.de/projekte/link-preview-with-open-graph/
+ * Description:       Fetches a website and creates a preview with OpenGraph.
+ * Version:           3.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Tequilian
+ * Author URI:        https://tequilian.de/
+ * Text Domain:       tequilian-link-preview
+ * License:           MIT License
+ * License URI:       https://tldrlegal.com/license/mit-license
+ */
