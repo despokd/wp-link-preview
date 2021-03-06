6cbe7d08a1e74cf186be91f0e4dadc0ce2a1c0c5
Kilian Domaratius <github@tequilian.de>
2021-03-06 12:59:24 +0100

    Typos

--- tequilian-link-preview/tequilian-link-preview.php
+++ tequilian-link-preview/tequilian-link-preview.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Plugin Name
+ * Link Preview by Tequilian
  *
  * @package           TequilianLinkPreview
  * @author            Kilian Domaratius
@@ -9,7 +9,7 @@
  * @license           MIT License
  *
  * @wordpress-plugin
- * Plugin Name:       Link Preview of Tequilian
+ * Plugin Name:       Link Preview by Tequilian
  * Plugin URI:        https://tequilian.de/projekte/link-preview-with-open-graph/
  * Description:       Fetches a website and creates a preview with OpenGraph.
  * Version:           3.0.0
@@ -17,7 +17,7 @@
  * Requires PHP:      7.2
  * Author:            Tequilian
  * Author URI:        https://tequilian.de/
- * Text Domain:       tequilian-link-preview
+ * Text Domain:       /projekte/link-preview-with-open-graph/
  * License:           MIT License
  * License URI:       https://tldrlegal.com/license/mit-license
  */
